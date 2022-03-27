7aa4c7afafe456a4d237b8b566eb66ac75bc8513
eoho15 <72741070+eoho15@users.noreply.github.com>
2022-03-27 23:55:24 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int ch = scan.next().charAt(0);
+
+        System.out.println(ch);
+
+    }
+}
