27dd272fad79e9c5c30d206c2486b0da8d661024
eoho15 <72741070+eoho15@users.noreply.github.com>
2022-04-30 17:50:30 +0900

    [Bronze II] Title: 최댓값, Time: 208 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int temp = 0;
 
-        int arr[] = new int[9];
-        for (int i = 0; i < arr.length; i++)
+        int[] arr = new int[9];
+        int max = 0;
+        int cnt = 0;
+
+        for(int i=0;i<9;i++) {
             arr[i] = scan.nextInt();
-
-        int Max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= Max)
-                Max = arr[i];
+            if(arr[i]>max) {
+                max = arr[i];
+                   cnt = i;
+            }
         }
-        System.out.println(Max);
 
-        for (int i = 0; i < arr.length; i++) {
+        System.out.println(max);
+        System.out.println(cnt+1);
 
-            if (arr[i] == Max)
-                temp = i;
-        }
-        System.out.println(temp + 1);
 
     }
 }
